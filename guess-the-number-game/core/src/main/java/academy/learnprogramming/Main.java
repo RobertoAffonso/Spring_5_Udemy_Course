0e1908df0d15168f78d217222f9dea6e9035a408
Roberto Affonso <Roberto.Affonso0611@gmail.com>
2018-07-11 14:45:55 -0300

    Initial Commit, including the first documents

---
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Roberto Affonso, created on 7/11/18
+ **/
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+    }
+}
